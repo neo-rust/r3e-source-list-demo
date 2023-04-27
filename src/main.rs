13b65957edb9b62be2acdd14950ce0d665734abe
txhsl <799498265@qq.com>
2023-04-27 17:52:23 +0800

    init commit

---
+++ src/main.rs
@@ -0,0 +1,276 @@
+use ethereum_types::{U256};
+use serde::Deserialize;
+
+trait OracleSource {
+    fn fetch(&self, params: Vec<u8>, callBackKey: String) -> Result<U256, reqwest::Error>;
+}
+
+#[derive(Debug)]
+pub struct TimeSourceAdapter {
+    pub name: String,
+}
+
+impl TimeSourceAdapter {
+    pub fn new(name: String) -> Self {
+        TimeSourceAdapter {
+            name,
+        }
+    }
+}
+
+impl OracleSource for TimeSourceAdapter {
+    fn fetch(&self, params: Vec<u8>, callBackKey: String) -> Result<U256, reqwest::Error> {
+        let time = std::time::SystemTime::now()
+            .duration_since(std::time::UNIX_EPOCH)
+            .unwrap()
+            .as_secs();
+        Ok(U256::from(time))
+    }
+}
+
+#[derive(Debug)]
+pub struct RngSourceAdapter {
+    pub name: String,
+}
+
+impl RngSourceAdapter {
+    pub fn new(name: String) -> Self {
+        RngSourceAdapter {
+            name,
+        }
+    }
+}
+
+impl OracleSource for RngSourceAdapter {
+    fn fetch(&self, params: Vec<u8>, callBackKey: String) -> Result<U256, reqwest::Error> {
+        //Ok(U256::from(new_stdrng().unwrap().gen::<u64>()))
+        Ok(U256::from(1))
+    }
+}
+
+#[derive(Debug)]
+pub struct ExchangeSourceAdapter {
+    pub name: String,
+    pub url: String,
+    pub params: Vec<String>,
+    pub base: Vec<String>,
+    pub quote: Vec<String>,
+}
+
+impl ExchangeSourceAdapter {
+    pub fn new(name: String, url: String, params: Vec<String>, base: Vec<String>, quote: Vec<String>) -> Self {
+        ExchangeSourceAdapter {
+            name,
+            url,
+            params,
+            base,
+            quote,
+        }
+    }
+}
+
+impl OracleSource for ExchangeSourceAdapter {
+    fn fetch(&self, params: Vec<u8>, callBackKey: String) -> Result<U256, reqwest::Error> {
+        let url = self.url.clone();
+        for param in &self.params {
+            let url = url.replacen("{}", &param, 1);
+        }
+        // let rpc_result = request_data(&url)
+        //     .unwrap()
+        //     .get(callBackKey)
+        //     .unwrap()
+        //     .as_f64()
+        //     .unwrap();
+        let rpc_result = 1;
+        Ok(ethereum_types::U256::from(rpc_result as u64))
+    }
+}
+
+#[derive(Debug)]
+pub struct CustomSourceAdapter {
+    pub name: String,
+    pub url: String,
+}
+
+impl CustomSourceAdapter {
+    pub fn new(name: String, url: String) -> Self {
+        CustomSourceAdapter {
+            name,
+            url,
+        }
+    }
+}
+
+impl OracleSource for CustomSourceAdapter {
+    fn fetch(&self, params: Vec<u8>, callBackKey: String) -> Result<U256, reqwest::Error> {
+        // let rpc_result = request_data(&self.url)
+        //     .unwrap()
+        //     .get(callBackKey)
+        //     .unwrap()
+        //     .as_f64()
+        //     .unwrap();
+        let rpc_result = 1;
+        Ok(ethereum_types::U256::from(rpc_result as u64))
+    }
+}
+#[derive(Debug, Deserialize)]
+struct SourceList {
+    sources: Vec<SourceConfig>,
+}
+
+#[derive(Debug, Deserialize)]
+struct SourceConfig {
+    name: String,
+    url: String,
+    params: Vec<String>,
+    base: Vec<String>,
+    quote: Vec<String>,
+}
+
+impl SourceList {
+    pub fn new(sources: String) -> Self {
+        let list: SourceList = toml::from_str(&sources).unwrap();
+
+        for x in &list.sources {
+            println!("{:?}", x.url);
+        }
+
+        list
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_time_adapter_new() {
+        let adapter = TimeSourceAdapter::new("time".to_string());
+        assert_eq!(adapter.name, "time");
+    }
+
+    #[test]
+    fn test_rng_adapter_new() {
+        let adapter = RngSourceAdapter::new("rng".to_string());
+        assert_eq!(adapter.name, "rng");
+    }
+
+    #[test]
+    fn test_exchange_adapter_new() {
+        let adapter = ExchangeSourceAdapter::new(
+            "cryptocompare".to_string(),
+            "https://min-api.cryptocompare.com/data/price?api_key={}&fsym={}&tsyms={}".to_string(),
+            ["d4cf504725efe27b71ec7d213f5db583ef56e88cfbf437a3483d6bb43e9839ab".to_string()].to_vec(),
+            ["BTC".to_string(), "ETH".to_string(), "USDT".to_string(), "USDC".to_string(), "BNB".to_string(), "XRP".to_string(), "BUSD".to_string()].to_vec(),
+            [
+                "BTC".to_string(),
+                "ETH".to_string(),
+                "USDT".to_string(),
+                "USDC".to_string(),
+                "BNB".to_string(),
+                "XRP".to_string(),
+                "BUSD".to_string(),
+                "DOGE".to_string(),
+                "ADA".to_string(),
+                "MATIC".to_string(),
+            ].to_vec()
+        );
+
+        assert_eq!(adapter.name, "cryptocompare");
+    }
+
+    #[test]
+    fn test_custom_adapter_new() {
+        let adapter = CustomSourceAdapter::new(
+            "cryptocompare".to_string(),
+            "https://min-api.cryptocompare.com/data/price?api_key=d4cf504725efe27b71ec7d213f5db583ef56e88cfbf437a3483d6bb43e9839ab&fsym=BTC&tsyms=ETH".to_string(),
+        );
+
+        assert_eq!(adapter.name, "cryptocompare");
+    }
+
+    #[test]
+    fn test_time_adapter_fetch() {
+        let adapter = TimeSourceAdapter::new("time".to_string());
+        let result = adapter.fetch(vec![], "".to_string());
+        assert_eq!(result.is_ok(), true);
+    }
+
+    #[test]
+    fn test_rng_adapter_fetch() {
+        let adapter = RngSourceAdapter::new("rng".to_string());
+        let result = adapter.fetch(vec![], "".to_string());
+        assert_eq!(result.is_ok(), true);
+    }
+
+    #[test]
+    fn test_exchange_adapter_fetch() {
+        let adapter = ExchangeSourceAdapter::new(
+            "cryptocompare".to_string(),
+            "https://min-api.cryptocompare.com/data/price?api_key={}&fsym={}&tsyms={}".to_string(),
+            ["d4cf504725efe27b71ec7d213f5db583ef56e88cfbf437a3483d6bb43e9839ab".to_string()].to_vec(),
+            ["BTC".to_string(), "ETH".to_string(), "USDT".to_string(), "USDC".to_string(), "BNB".to_string(), "XRP".to_string(), "BUSD".to_string()].to_vec(),
+            [
+                "BTC".to_string(),
+                "ETH".to_string(),
+                "USDT".to_string(),
+                "USDC".to_string(),
+                "BNB".to_string(),
+                "XRP".to_string(),
+                "BUSD".to_string(),
+                "DOGE".to_string(),
+                "ADA".to_string(),
+                "MATIC".to_string(),
+            ].to_vec()
+        );
+        let result = adapter.fetch([1, 2].to_vec(), "ETH".to_string());
+        assert_eq!(result.is_ok(), true);
+    }
+
+    #[test]
+    fn test_custom_adapter_fetch() {
+        let adapter = CustomSourceAdapter::new(
+            "cryptocompare".to_string(),
+            "https://min-api.cryptocompare.com/data/price?api_key=d4cf504725efe27b71ec7d213f5db583ef56e88cfbf437a3483d6bb43e9839ab&fsym=BTC&tsyms=ETH".to_string(),
+        );
+        let result = adapter.fetch(vec![], "ETH".to_string());
+        assert_eq!(result.is_ok(), true);
+    }
+
+    #[test]
+    fn test_load_list() {
+        let list = SourceList::new(
+            r#"
+            [[sources]]
+            name = "cryptocompare"
+            url = "https://min-api.cryptocompare.com/data/price?api_key={}&fsym={}&tsyms={}"
+            params = ["d4cf504725efe27b71ec7d213f5db583ef56e88cfbf437a3483d6bb43e9839ab"]
+            base = ["BTC", "ETH", "USDT", "USDC", "BNB", "XRP", "BUSD"]
+            quote = ["BTC", "ETH", "USDT", "USDC", "BNB", "XRP", "BUSD", "DOGE", "ADA", "MATIC"]
+            [[sources]]
+            name = "cryptocompare"
+            url = "https://min-api.cryptocompare.com/data/price?api_key={}&fsym={}&tsyms={}"
+            params = ["d4cf504725efe27b71ec7d213f5db583ef56e88cfbf437a3483d6bb43e9839ab"]
+            base = ["BTC", "ETH", "USDT", "USDC", "BNB", "XRP", "BUSD"]
+            quote = ["BTC", "ETH", "USDT", "USDC", "BNB", "XRP", "BUSD", "DOGE", "ADA", "MATIC"]
+        "#.to_string(),
+        );
+
+        for source in &list.sources {
+            let adapter = ExchangeSourceAdapter::new(
+                source.name.clone(),
+                source.url.clone(),
+                source.params.clone(),
+                source.base.clone(),
+                source.quote.clone(),
+            );
+            assert_eq!(adapter.base.len(), 7);
+            assert_eq!(adapter.quote.len(), 10);
+        }
+        assert_eq!(list.sources.len(), 2);
+    }
+}
